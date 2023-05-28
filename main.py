7ef66b004f559e70660398bd280f7b22678673b3
PeanutSplash <b1300658700@outlook.com>
2023-05-28 18:37:09 +0800

    modified:   main.py 	modified:   templates/index.html 	modified:   templates/login.html 	modified:   templates/redeem.html

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import requests
 import os
 import uuid
 import yaml
+import io
+import base64
 from io import BytesIO
 from flask import Flask, make_response, redirect, render_template, request, session
 from sqlalchemy import create_engine, select
@@ -104,6 +106,56 @@ engine = create_engine(
 Base.metadata.create_all(bind=engine, checkfirst=True)
 
 # 验证码实现类
+# class imageCode():
+#     def rndColor(self):
+#         '''随机颜色'''
+#         return (random.randint(32, 127), random.randint(32, 127), random.randint(32, 127))
+
+#     def geneText(self):
+#         '''生成4位验证码'''
+#         return ''.join(random.sample(string.ascii_letters + string.digits, 4)) #ascii_letters是生成所有字母 digits是生成所有数字0-9
+
+#     def drawLines(self, draw, num, width, height):
+#         '''划线'''
+#         for num in range(num):
+#             x1 = random.randint(0, width / 2)
+#             y1 = random.randint(0, height / 2)
+#             x2 = random.randint(0, width)
+#             y2 = random.randint(height / 2, height)
+#             draw.line(((x1, y1), (x2, y2)), fill='black', width=1)
+
+#     def getVerifyCode(self):
+#         '''生成验证码图形'''
+#         code = self.geneText()
+#         # 图片大小120×50
+#         width, height = 120, 50
+#         # 新图片对象
+#         im = Image.new('RGB', (width, height), 'white')
+#         # 字体
+#         font = ImageFont.truetype('app/static/arial.ttf', 40)
+#         # draw对象
+#         draw = ImageDraw.Draw(im)
+#         # 绘制字符串
+#         for item in range(4):
+#             draw.text((5 + random.randint(-3, 3) + 23 * item, 5 + random.randint(-3, 3)),
+#                     text=code[item], fill=self.rndColor(), font=font)
+#         # 划线
+#         self.drawLines(draw, 2, width, height)
+#         return im, code
+
+#     def getImgCode(self):
+#         image, code = self.getVerifyCode()
+#         # 图片以二进制形式写入
+#         buf = BytesIO()
+#         image.save(buf, 'jpeg')
+#         buf_str = buf.getvalue()
+#         # 把buf_str作为response返回前端，并设置首部字段
+#         response = make_response(buf_str)
+#         response.headers['Content-Type'] = 'image/gif'
+#         # 将验证码字符串储存在session中
+#         session['imageCode'] = code
+#         return response
+
 class imageCode():
     def rndColor(self):
         '''随机颜色'''
@@ -143,13 +195,18 @@ class imageCode():
 
     def getImgCode(self):
         image, code = self.getVerifyCode()
-        # 图片以二进制形式写入
-        buf = BytesIO()
-        image.save(buf, 'jpeg')
-        buf_str = buf.getvalue()
-        # 把buf_str作为response返回前端，并设置首部字段
-        response = make_response(buf_str)
-        response.headers['Content-Type'] = 'image/gif'
+        # 将图片转换为byte类型的数据
+        buf = io.BytesIO()
+        image.save(buf, format='jpeg')
+        buf_bytes = buf.getvalue()
+
+        # 将 byte 类型转为 base64 字符串
+        img_base64 = base64.b64encode(buf_bytes).decode('utf-8')
+
+        # 返回包含base64格式图片字符串的response对象
+        response = make_response(img_base64)
+        response.headers['Content-Type'] = 'text/plain'
+        
         # 将验证码字符串储存在session中
         session['imageCode'] = code
         return response
